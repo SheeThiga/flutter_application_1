46ac703ad31efc52f0bdd3921877053217b1f92d
Mercy Shee <thigamercy399@gmail.com>
2024-02-18 03:36:49 -0600

    updated output text

--- lib/controllers/calculator_controller.dart
+++ lib/controllers/calculator_controller.dart
@@ -2,7 +2,12 @@ import 'package:get/get.dart';
 
 class CalculatorController extends GetxController {
   var sum = 0.0.obs;
-  updateSum(double newSum) {
+  var num1 = 0.0.obs;
+  var num2 = 0.0.obs;
+
+  updateSum(double num1, double num2, double newSum) {
+    this.num1.value = num1;
+    this.num2.value = num2;
     sum.value = newSum;
   }
 }
--- lib/views/calculator.dart
+++ lib/views/calculator.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/controllers/calculator_controller.dart';
 import 'package:flutter_application_1/views/widgets/custom_button.dart';
 import 'package:flutter_application_1/views/widgets/custom_field.dart';
 import 'package:flutter_application_1/views/widgets/custom_text.dart';
 import 'package:get/get.dart';
 
-
 class Calculator extends StatelessWidget {
-  
   final TextEditingController num1controller = TextEditingController();
   final TextEditingController num2controller = TextEditingController();
-  double sum = 0.0;
+  final CalculatorController calculatorController =
+      Get.put(CalculatorController());
+     
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(      
         body: Container(
       padding: const EdgeInsets.all(20),
       child: Column(
@@ -33,15 +34,19 @@ class Calculator extends StatelessWidget {
             onPressed: () {
               double a = double.parse(num1controller.text);
               double b = double.parse(num2controller.text);
-              sum = a + b;
-              print("sum is: $sum");
+              
+              double s = a + b;
+              calculatorController.updateSum(a,b,s);
             },
             child: const Text("calculate"),
           ),
 
-          Obx(() => 
-             CustomText(
-                label: 'Sum is: $sum', labelColor: Colors.black, fontSize: 20.0),
+          Obx(
+            () => CustomText(
+                label:
+                    'The Sum of ${calculatorController.num1} and ${calculatorController.num2} is: ${calculatorController.sum}',
+                labelColor: Colors.black,
+                fontSize: 20.0),
           ),
         ],
       ),
